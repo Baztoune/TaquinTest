120c77e223464520a0a0e6d935b1b7fd8e33e888
Bastien Colmard <baztoune@gmail.com>
2014-04-26 00:05:52 +0200

    minimal CSS

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ function buildGrid(width, height){
 	var grid = document.getElementById('grid');
 	for(var i = 0; i < width; i++){
 		var newRow = document.createElement('div');
-	    newRow.setAttribute('class','row');
+	    newRow.setAttribute('class','row clearfix');
 		for(var j = 0; j < height; j++){
 	    	var newCell = document.createElement('div');
 	    	newCell.setAttribute('class','cell');
+	    	newCell.innerHTML = i*4 + j + 1;
 	    	newRow.appendChild(newCell);
 	    }
 		grid.appendChild(newRow);
