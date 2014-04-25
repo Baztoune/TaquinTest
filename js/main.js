4da86237bd7b2d46863c9d2b7c4cce44c2301793
Bastien Colmard <baztoune@gmail.com>
2014-04-26 01:31:12 +0200

    start dragdrop

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-
-function buildGrid(rows, cols){
+function buildGrid(rows, cols) {
 	var grid = document.getElementById('grid');
-	for(var i = 0; i < rows; i++){
+	for (var i = 0; i < rows; i++) {
 		var newRow = document.createElement('div');
-	    newRow.setAttribute('class','row clearfix');
-		for(var j = 0; j < cols; j++){
-	    	var newCell = document.createElement('div');
-	    	newCell.setAttribute('class','cell cell-' + i + '-' + j);
-	    	newCell.innerHTML = i*4 + j + 1;
-	    	newRow.appendChild(newCell);
-	    }
+		newRow.setAttribute('class', 'row clearfix');
+		for (var j = 0; j < cols; j++) {
+			var newCell = document.createElement('div');
+			newCell.classList.add('cell');
+
+			var newTile = document.createElement('div');
+			newTile.innerHTML = i * 4 + j + 1;
+			newTile.classList.add('tile', 'tile-' + i + '-' + j);
+			newTile.setAttribute('draggable', 'true');
+
+			newCell.appendChild(newTile);
+			newRow.appendChild(newCell);
+
+			newCell.addEventListener('dragover', function(e) {
+				this.classList.add('over');
+			});
+			newCell.addEventListener('dragleave', function(e) {
+				this.classList.remove('over');
+				console.log(this);
+			});
+			newCell.addEventListener('drop', function(e) {
+				this.classList.remove('over');
+				console.log(this);
+			});
+		}
 		grid.appendChild(newRow);
 	}
 };
