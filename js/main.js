ab6e0c637ff4f39235897a4baf71d68b51a94b27
Bastien Colmard <baztoune@gmail.com>
2014-04-27 18:24:27 +0200

    big refactoring, add comments

--- js/main.js
+++ js/main.js
@@ -6,29 +6,30 @@ function Game() {
 	var draggedTile = null;
 }
 
+/* First call, triggers the grid building */
 Game.prototype.init = function init() {
 	console.log('starting the game');
 	this.buildView();
+	this.attachDragDropEvents();
 };
 
+/* Shuffle the grid */
 Game.prototype.shuffle = function shuffle() {
 	// to make sure we have a solvable grid, we start with solved grid and make 100 random moves
 	// could be optimized
 	console.log('shuffle called');
-
 	var game = this;
-	console.log('vcfvf111');
+
 	for(var i=0;i<conf.shuffleMoves;i++){
-		console.log('vcfvf');
 		game.randomMove();
 	}
 };
 
+/* Check if the game is solved by comparing the tiles positions with the cells positions */
 Game.prototype.detectGameSolved = function detectGameSolved() {
-	var game = this;
 	var solved = true;
 
-	game.grid.cells.forEach(function(row){
+	this.grid.cells.forEach(function(row){
 		row.forEach(function(cell){
 			if(cell.tile){
 				// if the cell has a tile
@@ -47,8 +48,7 @@ Game.prototype.detectGameSolved = function detectGameSolved() {
 
 /* build the view from the model */
 Game.prototype.buildView = function buildView() {
-	var game = this;
-	var cells = game.grid.cells;
+	var cells = this.grid.cells;
 
 	var grid = document.getElementById('grid');
 	cells.forEach(function(col) {
@@ -61,7 +61,6 @@ Game.prototype.buildView = function buildView() {
 			newCell.id='cell-' + cell.x + '-' + cell.y;
 			newCell.cellObject=cell;
 			newRow.appendChild(newCell);
-			game.attachDragEventsOnDroppableElement(newCell);
 
 			/* Build tile */
 			if(cell.tile) {
@@ -78,102 +77,29 @@ Game.prototype.buildView = function buildView() {
 
 				newCell.appendChild(newTile);
 
-				game.attachDragEventsOnDraggableElement(newTile);
 			}
 		});
 		grid.appendChild(newRow);
 	});
 }
 
-Game.prototype.attachDragEventsOnDraggableElement = function attachDragEventsOnDraggableElement(el) {
-	var game = this;
-	el.addEventListener('dragstart', function(e) {
-		// pass the id, so we can get the element on drop
-    	e.dataTransfer.setData('Text', this.id);
-    	game.draggedTile = this.tileObject;
-	}, false);
+/* Calls the attachDragDropEvents on each cell and tile */
+Game.prototype.attachDragDropEvents = function attachDragDropEvents(){
+	this.grid.cells.forEach(function(col) {
+		col.forEach(function(cell) {
+			// for every cell, we attach the events
+			cell.attachDragDropEvents(this);
+			if(cell.tile){
+				// if the tile exists, we attach the events
+				cell.tile.attachDragDropEvents(this);
+			}
+		});
+	});
+}
 
-	el.addEventListener('dragenter', function(e) {
-		// prevent event from propagating (otherwise dragenter target is the tile)
-		e.stopPropagation();
-	}, false);
-
-	/*Awesome fix for IE9 http://stackoverflow.com/questions/5500615/internet-explorer-9-drag-and-drop-dnd#comment11341167_8986075*/
-	el.addEventListener('selectstart', function(e){
-		this.dragDrop(); 
-		return false;
-	}, false);
-
-	el.addEventListener('dblclick', function(e) {
-		// half of the time, we open a link or a video
-		if(getRandomInt(0,100)%2){
-			// open youtube popin
-			console.log('opening video Pop-in');
-			showVideo();
-		} else {
-			// open link in new tab
-			console.log('opening website in new tab');
-			window.open(conf.linkUrl,'_blank');
-		}
-	}, false);
-};
-
-Game.prototype.attachDragEventsOnDroppableElement = function attachDragEventsOnDroppableElement(el) {
-	var game = this; // cache before event handlers override it
-
-	el.addEventListener('dragenter', function(e) {
-		if (e.preventDefault) {
-			e.preventDefault();
-		}
-		var dstCell = this.cellObject;
-
-		if(dstCell.acceptDrop(game.draggedTile)) {
-			this.classList.add('accept-drop');
-		}
-
-		return false;
-	}, false);
-	el.addEventListener('dragover', function(e) {
-		if (e.preventDefault) {
-			e.preventDefault();
-		}
-		return false;
-	}, false);
-	el.addEventListener('dragleave', function(e) {
-		if (e.preventDefault) {
-			e.preventDefault();
-		}
-		this.classList.remove('accept-drop');
-		return false;
-	}, false);
-	el.addEventListener('drop', function(e) {
-	    if (e.preventDefault) {
-	    	e.preventDefault();
-	    }
-
-		if(this.cellObject.acceptDrop(game.draggedTile)) {
-			game.draggedTile.moveTo(this.cellObject);
-		}
-
-		this.classList.remove('accept-drop');
-		game.draggedTile=null;
-
-		return false;
-	}, false);
-};
-
-Game.prototype.moveTile = function moveTile(el, dest) {
-	/* Clear cell */
-	while (dest.firstChild) {
-		dest.removeChild(dest.firstChild);
-	}
-	dest.appendChild(el);
-};
-
+/* Make a random move */
 Game.prototype.randomMove = function randomMove() {
-	var game = this;
-	var grid = game.grid;
-	var emptyCell = game.grid.getEmptyCell();
+	var emptyCell = this.grid.getEmptyCell();
 
 	// pick a move
 	emptyCell.getRandomAdjacentCell().tile.moveTo(emptyCell, true);
@@ -200,6 +126,8 @@ function Grid(rows, cols) {
 		this.cells.push(row);
 	}
 }
+
+/* Return the only empty cell of the grid */
 Grid.prototype.getEmptyCell = function getEmptyCell() {
 	var emptyCell;
 	this.cells.forEach(function(col) {
@@ -212,6 +140,7 @@ Grid.prototype.getEmptyCell = function getEmptyCell() {
 	return emptyCell;
 };
 
+
 /*
  * Cell object
  */
@@ -221,11 +150,15 @@ function Cell(x, y) {
 	this.x = x;
 	this.y = y;
 }
+
+/* Test if the cell is the last one of the grid (bottom right) */
 Cell.prototype.isLast = function isLast() {
 	if(this.x + 1 == conf.rows && this.y + 1 == conf.cols) {
 		return true;
 	}
 };
+
+/* Test if the cell is empty and can accept this tile */
 Cell.prototype.acceptDrop = function acceptDrop(tile) {
 	var dstCell = this;
 	var srcCell = tile.cell;
@@ -237,12 +170,16 @@ Cell.prototype.acceptDrop = function acceptDrop(tile) {
 	console.log('nop, can\'t drop');
 	return false;
 };
+
+/* Test if the cell is empty */
 Cell.prototype.isEmpty = function isEmpty() {
 	if(!this.tile) {
 		return true;
 	}
 	return false;
 };
+
+/* Test if the cell has a common border with the other one */
 Cell.prototype.isAdjacentTo = function isAdjacentTo(otherCell) {
 	var sameRow = this.y==otherCell.y;
 	var sameCol = this.x==otherCell.x;
@@ -253,18 +190,69 @@ Cell.prototype.isAdjacentTo = function isAdjacentTo(otherCell) {
 	return false;
 };
 
+/* Get the DOM element binded to this cell object */
 Cell.prototype.getDomElement = function getDomElement() {
 	var elt = document.getElementById('cell-'+this.x+'-'+this.y);
 	return elt;
 };
+
+/* Empty the tile of this cell */
 Cell.prototype.clear = function clear() {
 	this.tile=null;
 };
+
+/* Attach the dragdrop events to the DOM element of the cell */
+Cell.prototype.attachDragDropEvents = function attachDragDropEvents() {
+	var elt = this.getDomElement();
+
+	elt.addEventListener('dragenter', function(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+		var dstCell = this.cellObject;
+
+		if(dstCell.acceptDrop(game.draggedTile)) {
+			this.classList.add('accept-drop');
+		}
+
+		return false;
+	}, false);
+	elt.addEventListener('dragover', function(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+		return false;
+	}, false);
+	elt.addEventListener('dragleave', function(e) {
+		if (e.preventDefault) {
+			e.preventDefault();
+		}
+		this.classList.remove('accept-drop');
+		return false;
+	}, false);
+	elt.addEventListener('drop', function(e) {
+	    if (e.preventDefault) {
+	    	e.preventDefault();
+	    }
+
+		if(this.cellObject.acceptDrop(game.draggedTile)) {
+			game.draggedTile.moveTo(this.cellObject);
+		}
+
+		this.classList.remove('accept-drop');
+		game.draggedTile=null;
+
+		return false;
+	}, false);
+};
+
+/* Return any of the available adjacent cells of this cell*/
 Cell.prototype.getRandomAdjacentCell = function getRandomAdjacentCell() {
 	var max_x = conf.cols-1 ;
 	var max_y = conf.rows-1 ;
 
 	var availableCells = [];
+	/* We check for every direction */
 	if(this.x-1>=0){
 		availableCells.push(game.grid.cells[this.x-1][this.y]);
 	}
@@ -278,7 +266,7 @@ Cell.prototype.getRandomAdjacentCell = function getRandomAdjacentCell() {
 		availableCells.push(game.grid.cells[this.x][this.y+1]);
 	}
 
-	// return any of the available cells
+	/* return any of the available cells */
 	return availableCells[getRandomInt(0,availableCells.length-1)];
 };
 
@@ -291,36 +279,67 @@ function Tile(x,y) {
 	this.y=y;
 	var cell;
 }
+
+/* Get the DOM element binded to this tile object */
 Tile.prototype.getDomElement = function getDomElement() {
 	return this.cell.getDomElement().firstChild;
 };
+
+/* Move the tile DOM element to its destination. Update the model */
 Tile.prototype.moveTo = function moveTo(cell, skipSolutionDetection) {
-	console.log('tile will be moved from ',this.cell);
 	cell.getDomElement().appendChild(this.getDomElement());
 	this.cell.clear();
 	this.cell=cell;
 	this.cell.tile=this;
 
 	if(!skipSolutionDetection){
+		// we skip the detection when we shuffle the grid
 		game.detectGameSolved();
 	}
-	console.log('tile moved to',this.cell );
 };
 
+/* Attach the dragdrop events to the DOM element of the tile */
+Tile.prototype.attachDragDropEvents = function attachDragDropEvents() {
+	var elt = this.getDomElement();
+
+	elt.addEventListener('dragstart', function(e) {
+		// pass the id, so we can get the element on drop
+    	e.dataTransfer.setData('Text', this.id);
+    	game.draggedTile = this.tileObject;
+	}, false);
+
+	elt.addEventListener('dragenter', function(e) {
+		// prevent event from propagating (otherwise dragenter target is the tile)
+		e.stopPropagation();
+	}, false);
+
+	elt.addEventListener('selectstart', function(e){
+		// Awesome fix for IE9 
+		// http://stackoverflow.com/questions/5500615/internet-explorer-9-drag-and-drop-dnd#comment11341167_8986075
+		this.dragDrop(); 
+		return false;
+	}, false);
+
+	elt.addEventListener('dblclick', function(e) {
+		// half of the time, we open a link or a video
+		if(getRandomInt(0,100)%2){
+			// open youtube popin
+			console.log('opening video Pop-in');
+			showVideo();
+		} else {
+			// open link in new tab
+			console.log('opening website in new tab');
+			window.open(conf.linkUrl,'_blank');
+		}
+	}, false);
+};
 
 /* Returns a random integer between min and max */
 var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/*Start game*/
-var game = new Game();
-game.init();
-var button = document.getElementById('shuffle-button');
-button.addEventListener('click',function(){
-	game.shuffle();
-});
-
+/* Build the video player and show the modal window */
 var showVideo = function(){
 	/* Build youtube player */
 	var ytContainer = document.getElementById('youtubePlayerContainer');
@@ -340,6 +359,7 @@ var showVideo = function(){
 	document.getElementsByClassName('overlay')[0].addEventListener('click', hideVideo);
 };
 
+/* Destroy the player and hide the modal */
 var hideVideo = function(){
 	/* Destroy youtube player*/
 	document.getElementById('youtubePlayerContainer').innerHTML = ''; // clear
@@ -348,3 +368,16 @@ var hideVideo = function(){
 	document.getElementsByClassName('modal')[0].style.display='none';
 	document.getElementsByClassName('overlay')[0].style.display='none';
 };
+
+
+var game;
+(function(){
+	/*Start game*/
+	game = new Game();
+	game.init();
+	var button = document.getElementById('shuffle-button');
+	button.addEventListener('click',function(){
+		game.shuffle();
+	});
+})();
+
